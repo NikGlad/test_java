8912cf82f8b78f3c5683bc37d6d83b0720c8a1fa
NikGlad <90272508+NikGlad@users.noreply.github.com>
2024-07-31 21:16:21 +0300

    31_07

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+//Основные оперерации, синтаксис
+
 //public class Main {
 //
 //    public static void main(String[] args) {
@@ -20,9 +22,65 @@
 
  */
 
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        System.out.println("Hello\"\n\tWorld!");
+//    }
+//}
+
+//Переменные и типы данных
+
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        System.out.println("Переменные и типы данных");
+//
+////      int age;
+//        int age = 56;
+////        age = 55;
+//
+//
+//
+//        System.out.println(age);
+//        age = 45;
+//        System.out.println("Возраст " + age + '.');
+//
+//
+//    }
+//}
+
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        System.out.println("Переменные и типы данных");
+//
+//
+////        Как обозначить переменные в зависимоти от длины числа. Для byte меньше всего требуется оперативной памяти и соответственно самое короткое число можно прописать
+//        byte age = 56;
+//        short num = 22000;
+//        int num1 = 220000000;
+//        long num2 = 220000000;
+//
+//    }
+//}
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello\"\n\tWorld!");
+        System.out.println("Переменные и типы данных");
+
+//        переменная число с точкой
+        float num = 4.252552f;
+
+//        переменная один символ
+        char ch = '5';
+        //        переменная один символ
+        String user_name = "Aleks";
+
+//        переменная булевые значения
+        boolean isHappy = false;
+
+        System.out.println(isHappy);
     }
 }
