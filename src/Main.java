08280d79062ad06d34e50de486be26a7d09bc6ac
NikGlad <90272508+NikGlad@users.noreply.github.com>
2024-08-21 22:28:08 +0300

    21_08.3

--- src/Main.java
+++ src/Main.java
@@ -411,3 +411,19 @@ import java.util.Scanner;
 //        }
 //    }
 //}
+
+//кредитный калькулятор
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int  amount = scanner.nextInt();
+        for(int i = 0; i < 3; i++) {
+            amount = amount - (int) Math.ceil(amount*0.1);
+
+        }
+        System.out.println(amount);
+
+    }
+}
