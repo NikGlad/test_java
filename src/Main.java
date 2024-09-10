7a3a289820a2e391afed71039c975b4b213e6e23
NikGlad <90272508+NikGlad@users.noreply.github.com>
2024-09-10 21:52:18 +0300

    02_09

--- src/Main.java
+++ src/Main.java
@@ -308,8 +308,6 @@ import java.util.Scanner;
 //}
 
 
-
-
 //Цикл for
 
 //public class Main {
@@ -734,16 +732,76 @@ Share a Coke
 /*
 Стакан наполовину пуст или наполовину полон?
 */
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        double glass = 0.5;
+//        Scanner scanner = new Scanner(System.in);
+//        boolean i = scanner.nextBoolean();
+//        if (i == true)
+//            System.out.println((int) Math.ceil(glass));
+//        else if (i == false)
+//            System.out.println((int) Math.floor(glass));
+//    }
+//}
+
+/*
+Четные и нечетные ячейки массива
+*/
+
+//public class Main {
+//    public static final String ODD = "Нечётный";
+//    public static final String EVEN = "Чётный";
+//    public static String[] strings = new String[5];
+//
+//    public static void main(String[] args){
+//        strings[0] = EVEN;
+//        strings[1] = ODD;
+//        strings[2] = EVEN;
+//        strings[3] = ODD;
+//        strings[4] = EVEN;
+//
+//        System.out.print("index = 0");
+//        System.out.println(" value = " + strings[0]);
+//        System.out.print("index = 1");
+//        System.out.println(" value = " + strings[1]);
+//        System.out.print("index = 2");
+//        System.out.println(" value = " + strings[2]);
+//        System.out.print("index = 3");
+//        System.out.println(" value = " + strings[3]);
+//        System.out.print("index = 4");
+//        System.out.println(" value = " + strings[4]);
+//    }
+//}
+
+
+//смена знака на противоположный
 
 public class Main {
+    public static int[] array = new int[]{-1, 2, 3, -4, -5};
+
     public static void main(String[] args) {
-        double glass = 0.5;
-        Scanner scanner = new Scanner(System.in);
-        boolean i = scanner.nextBoolean();
-        if (i == true)
-            System.out.println((int)Math.ceil(glass));
-        else if (i == false)
-            System.out.println((int)Math.floor(glass));
+        if (array[0] % 2 == 0) {
+            array[0] = -array[0];
+        }
+        if (array[1] % 2 == 0) {
+            array[1] = -array[1];
+        }
+        if (array[2] % 2 == 0) {
+            array[2] = -array[2];
+        }
+        if (array[3] % 2 == 0) {
+            array[3] = -array[3];
+        }
+        if (array[4] % 2 == 0) {
+            array[4] = -array[4];
+        }
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
     }
 }
 
